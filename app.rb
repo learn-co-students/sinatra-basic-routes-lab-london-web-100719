80b7822c4b8df191dcb2de30d8f29733f51a69d3
github username <github email address>
2019-10-28 12:34:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/name' do
+       "My name is Bogo!"
+    end
+
+    get '/hometown' do
+        "My hometown is Reading!"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is too many to name"
+    end
 end
