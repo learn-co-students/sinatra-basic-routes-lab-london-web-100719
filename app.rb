36fc1620454e46f079a9c202ad5fb5f5a82f55eb
Peter Sampson <peter.sampson010@gmail.com>
2019-10-28 13:21:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Peter Sampson"
+    end 
+
+    get '/hometown' do  
+        "My hometown is Hometown"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is song3"
+    end 
+
 end
