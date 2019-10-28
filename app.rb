4126c0126afa6cacb852f08ae0b39abff4160ed1
Tish Chungoora <tishal82@gmail.com>
2019-10-28 10:05:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Tish"
+    end
+
+    get '/hometown' do
+        "My hometown is Quatre-Bornes"
+
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Emergent Evolution"
+    end
+
 end
